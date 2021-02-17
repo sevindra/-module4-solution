671ae8c06bb7d4e4c863c90dff24f81fb81f9602
sevindra <sevindra@gmail.com>
2021-02-17 14:24:00 +0530

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -13,7 +13,7 @@ var speakWordHello = "Hello";
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function SpeaksHello(name) {
+function helloSpeaker(name) {
   console.log(speakWordHello + " " + name);
 }
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
